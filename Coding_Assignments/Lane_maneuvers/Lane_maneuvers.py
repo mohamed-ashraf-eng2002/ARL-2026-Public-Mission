5d015a0f175cf5a7b392995600b0a1cdb32e14bd
mohamed-ashraf-eng2002 <mohamed.eng2002.ashraf@gmail.com>
2025-09-04 02:16:48 +0300

    Update Lane_maneuvers.py

--- Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
+++ Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
@@ -8,4 +8,23 @@ def analyze_drive(lanes: list[int]) -> tuple[int, int]:
     Returns:
         tuple[int, int]: (number of lane changes, number of dangerous maneuvers)
     """
-    pass
+    if not lanes or len(lanes) == 1: #If the lanes list is empty then pass through the function 
+        pass
+
+    #Initial values for the Lane Changes and the Dangerous Maneuvers
+    lane_changes = 0
+    dangerous_man = 0
+
+    #Looping through the provided list to check for Lane Changes and Dangerous Maneuvers
+    for i in range(1, len(lanes)):
+        previous = lanes[i - 1]
+        current = lanes[i]
+
+
+        if current != previous:
+            lane_changes += 1
+        if (previous == 0 and current == 2) or (previous == 2 and current == 0):
+            dangerous_man += 1
+
+
+    return (lane_changes, dangerous_man)
